37819abf7ce7aa4d95658ac6154cd71b46880b8a
Mykyta Pashura <mpashura@mpashura.local>
2017-09-29 09:59:27 +0300

    working on ch_6

--- ch_6.py
+++ ch_6.py
@@ -11,8 +11,23 @@ def main():
         code.write(response.content)
 
     channel_zip = zipfile.ZipFile('data/channel.zip')
-    print([i.filename for i in channel_zip.filelist])
-    print(channel_zip.read('readme.txt'))
+    # print([i.filename for i in channel_zip.filelist])
+    # print(channel_zip.read('readme.txt'))
+
+    # for i in channel_zip.filelist:
+    #     if '90052' in str(i):
+    #         print(channel_zip.open(i).read())
+
+    nothing = '90052'
+    # next_nothing = ''.join([i for i in response.text if i.isnumeric()])
+    response = str(channel_zip.open('{}.txt'.format(nothing)).read())
+    print(response)
+    while 'Next nothing' in str(channel_zip.open('{}.txt'.format(nothing)).read()):
+        response = channel_zip.open('{}.txt'.format(nothing)).read()
+        print(nothing)
+        print(response)
+        nothing = ''.join([i for i in response if i.isnumeric()])
+
 
 
 if __name__ == '__main__':
